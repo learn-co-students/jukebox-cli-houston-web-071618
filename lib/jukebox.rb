bfd8cf80dd106d560373224d01866f2c1af81364
Marylene Chan <marylenechan@marylenes-mbp-2.attlocal.net>
2018-07-10 10:12:14 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,7 @@ puts " - list : displays a list of songs you can play"
 puts " - play : lets you choose a song to play"
 puts " - exit : exits this program"
 end
-help
+# help
 
 def list(songs_a)
 counter = 0
@@ -26,17 +26,40 @@ songs_a.each do | song |
   puts "#{counter}. #{song}"
 end
 end
-list(songs)  
+# list(songs)  
 
 def play(songs_a)
 puts "Please enter a song name or number:"
-song_num = gets.chomp
-song_num = song_num.to_i - 1
-  if song_num < songs_a.length
-    puts "Playing " + songs_a[song_num]
+song_num = gets.chomp  
+song_to_play = song_num.to_i - 1
+# puts song_num
+  if song_to_play < songs_a.length
+    puts "Playing " + songs_a[song_to_play]
   elsif 
     puts "Invalid input, please try again."
   end  
 end
 # play(songs)  
+
+def exit_jukebox
+puts "Goodbye"
+end
+
+def run(songs_a)
+puts "Please enter a command:"
+user_command = gets.chomp
+  if user_command == "exit"
+    exit_jukebox
+  elsif user_command == "list"
+    list(songs_a)
+    run(songs_a)
+  elsif user_command == "play"
+    play(songs_a)
+    run(songs_a)
+  elsif user_command =="help"
+    help
+    run(songs_a)
+  end
+end  
+run(songs)
 
